233d1d7e543fa23c932b7d10ed21fe669161e2cf
trankhanhbao <tranbao22051999@gmail.com>
2024-03-24 18:48:44 +0700

    xong chapter 12: Module HomePage, video #104.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,75 @@
 import MainSlider from "@/components/main/main.slider";
 import { Container } from "@mui/material";
+// import { sendRequestJS } from '@/utils/old.api'
+import { sendRequest } from '@/utils/api'
 
-export default function HomePage() {
+export default async function HomePage() {
+  // Cách 1: kiểu thổ dân
+
+  // const res = await fetch('http://localhost:8000/api/v1/tracks/top', {
+  //   method: 'POST',
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     category: 'CHILL',
+  //     limit: 5
+  //   })
+  // })
+  // const data = res.json()
+  // console.log('>>> data: ', data)
+
+  // Cách 2:API wrapper fetch (dùng JS)
+
+  // const res = await sendRequestJS({
+  //   url: 'http://localhost:8000/api/v1/tracks/top',
+  //   method: 'POST',
+  //   body: {
+  //     category: 'CHILL',
+  //     limit: 5
+  //   }
+  // })
+  // console.log('>>>> res: ', res)
+
+  // Cách 3:API wrapper fetch (dùng TS)
+
+  const chills = await sendRequest<IBackendRes<ITrackTop[]>>({
+    url: 'http://localhost:8000/api/v1/tracks/top',
+    method: 'POST',
+    body: { category: 'CHILL', limit: 10 },
+
+  })
+
+  const workouts = await sendRequest<IBackendRes<ITrackTop[]>>({
+    url: 'http://localhost:8000/api/v1/tracks/top',
+    method: 'POST',
+    body: { category: 'WORKOUT', limit: 10 },
+
+  })
+
+  const party = await sendRequest<IBackendRes<ITrackTop[]>>({
+    url: 'http://localhost:8000/api/v1/tracks/top',
+    method: 'POST',
+    body: { category: 'PARTY', limit: 10 },
+
+  })
+  // console.log('>>>> res (TS): ', res.data[0].)
+
   return (
     <Container>
-      <MainSlider />
+      <MainSlider
+        title={'Top Chills'}
+        data={chills?.data ? chills.data : []}
+      // hoặc : data={chills?.data ?? []}
+      />
+      <MainSlider
+        title={'Top Workout'}
+        data={workouts?.data ?? []}
+      />
+      <MainSlider
+        title={'Top Party'}
+        data={party?.data ?? []}
+      />
     </Container>
   );
 }
--- src/components/main/main.slider.tsx
+++ src/components/main/main.slider.tsx
@@ -1,43 +1,63 @@
 'use client'
-
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import { Settings } from 'react-slick'
-import { Box, Button } from "@mui/material";
+import { Box, Button, Divider } from "@mui/material";
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 
-const MainSlider = () => {
+interface IProps {
+    data: ITrackTop[];
+    title: string;
+}
 
-    const NextArrow = (props: any) => {
-        return (
-            <Button
-                onClick={props.onClick}
-                sx={{
-                    position: 'absolute',
-                    zIndex: 1,
-                    top: '50%',
-                    minWidth: 30,
-                    width: 35,
-                    padding: 0
-                }}
-            >
-                Next
-            </Button>
-        )
-    }
+const MainSlider = (props: IProps) => {
+
+    const { data, title } = props
+
+    console.log('>>> checkData:', props.data)
 
     const PrevArrow = (props: any) => {
         return (
             <Button
+                color="inherit"
+                variant="contained"
                 onClick={props.onClick}
                 sx={{
+                    // border: '1px solid #1c78d3',
                     position: 'absolute',
+                    transform: 'translateY(-50%)',
                     zIndex: 1,
-                    right: 0,
-                    top: '50%',
+                    top: '25%',
+                    left: '-50px',
+                    minWidth: 30,
+                    width: 35,
                 }}
             >
-                Prev
+                <ChevronLeftIcon />
+            </Button>
+        )
+    }
+
+    const NextArrow = (props: any) => {
+        return (
+            <Button
+                color="inherit"
+                variant="contained"
+                onClick={props.onClick}
+                sx={{
+                    // border: '1px solid #1c78d3',
+                    position: 'absolute',
+                    transform: 'translateY(-50%)',
+                    top: '25%',
+                    right: '-50px',
+                    zIndex: 1,
+                    minWidth: 30,
+                    width: 35,
+                }}
+            >
+                <ChevronRightIcon />
             </Button>
         )
     }
@@ -48,29 +68,46 @@ const MainSlider = () => {
         speed: 500,
         slidesToShow: 5,
         slidesToScroll: 1,
+        prevArrow: <PrevArrow />,
         nextArrow: <NextArrow />,
-        prevArrow: <PrevArrow />
     }
     return (
-        <Box {...settings}>
-            <div >
-                <h3 style={{ textAlign: "center" }}>1</h3>
-            </div>
-            <div>
-                <h3 style={{ textAlign: "center" }}>2</h3>
-            </div>
-            <div>
-                <h3 style={{ textAlign: "center" }}>3</h3>
-            </div>
-            <div>
-                <h3 style={{ textAlign: "center" }}>4</h3>
-            </div>
-            <div>
-                <h3 style={{ textAlign: "center" }}>5</h3>
-            </div>
-            <div>
-                <h3 style={{ textAlign: "center" }}>6</h3>
-            </div>
+        <Box
+            sx={{
+                margin: '0 50px',
+                '.track': {
+                    padding: '0 10px',
+                    'img': {
+                        height: 150,
+                        width: 150
+                    }
+                },
+                'h3': {
+                    border: '1px solid #ccc',
+                    padding: '20px',
+                    margin: '0 10px',
+                    height: '192px',
+                    textAlign: 'center'
+                }
+            }}
+        >
+            <h2>{title}</h2>
+            <Slider {...settings}>
+                {data.map(track => {
+                    return (
+                        <div className="track" key={track._id}>
+                            <img src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/images/${track.imgUrl}`} />
+                            <h4>{track.title}</h4>
+                            <h5>{track.description}</h5>
+                        </div>
+                    )
+                })}
+                {/* <div className="abc">
+                    <h3>1</h3>
+                </div> */}
+            </Slider>
+            <Divider sx={{ marginTop: '50px' }} />
+
         </Box>
     );
 }
